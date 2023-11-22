ff7378ff8db06c289703fabd1873abdeac139dcd
Alek <ole.alexander89@gmail.com>
2023-11-22 11:23:30 +0100

    working prototype

--- src/fibonacci-carousel/index.tsx
+++ src/fibonacci-carousel/index.tsx
@@ -1,25 +1,70 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { calculateIndex } from "./utils";
 import "./style.css";
 
 export type FibonacciCarouselProps = {
-    imagesSources: string[]
+    imageSources: string[]
+    cycleTimeMillies?: number
 }
 
-export const FibonacciCarousel: FC<FibonacciCarouselProps> = ({imagesSources}: FibonacciCarouselProps ) => {
+export const FibonacciCarousel: FC<FibonacciCarouselProps> = ({imageSources, cycleTimeMillies=0}: FibonacciCarouselProps ) => {
 
-    let index = 0;
+
+    let [centerIndex, setCenterIndex] = useState(0);
+
+    const classes = [
+        "n-0-left img-cell",
+        "n-1-left img-cell",
+        "n-2-left img-cell",
+        "n-3-left img-cell",
+        "n-4 img-cell",
+        "n-3-right img-cell",
+        "n-2-right img-cell",
+        "n-1-right img-cell",
+        "n-0-right img-cell"
+    ]
+
+    useEffect(() => {
+        if (cycleTimeMillies > 0) {
+            setInterval(() => setCenterIndex(centerIndex--), cycleTimeMillies)
+        }
+    }, [])
 
     return (
+        <>
         <div className="galleryContainer">
-            <img className={"n-1-left"} src={imagesSources[calculateIndex(index, -3, imagesSources.length)]}/>
-            <img className={"n-2-left"} src={imagesSources[calculateIndex(index, -2, imagesSources.length)]}/>
-            <img className={"n-3-left"} src={imagesSources[calculateIndex(index, -1, imagesSources.length)]}/>
-            <img className={"n-4"} src={imagesSources[calculateIndex(index, 0, imagesSources.length)]}/>
-            <img className={"n-2-right"} src={imagesSources[calculateIndex(index, 2, imagesSources.length)]}/>
-            <img className={"n-1-right"} src={imagesSources[calculateIndex(index, 3, imagesSources.length)]}/>
-            <img className={"n-3-right"} src={imagesSources[calculateIndex(index, 1, imagesSources.length)]}/>
+            {imageSources.map((imageSource, index) => <img className={getClassName(index)} src={imageSource} onClick={() => {
+                const classIndex = getClassIndex(index);
+                if (classIndex === -1) return;
+                setCenterIndex(centerIndex+(classIndex-4));
+            }}/>)}
         </div>
+        <button onClick={() => setCenterIndex(centerIndex++) }>Tilbake</button>
+        <button onClick={() => setCenterIndex(centerIndex--) }>Frem</button>
+        </>
     )
+
+    function getClassName(imageIndex: number): string {
+        const classIndex = getClassIndex(imageIndex)
+        if (classIndex === -1) return "no-show";
+        return classes[classIndex];
+    }
+
+    function getClassIndex(imageIndex: number) {
+        let validYolo = [
+            calculateIndex(centerIndex, -4, imageSources.length),
+            calculateIndex(centerIndex, -3, imageSources.length),
+            calculateIndex(centerIndex, -2, imageSources.length),
+            calculateIndex(centerIndex, -1, imageSources.length),
+            calculateIndex(centerIndex, 0, imageSources.length),
+            calculateIndex(centerIndex, 1, imageSources.length),
+            calculateIndex(centerIndex, 2, imageSources.length),
+            calculateIndex(centerIndex, 3, imageSources.length),
+            calculateIndex(centerIndex, 4, imageSources.length)
+        ]
+        return validYolo.indexOf(imageIndex)
+    }
+    
+    
 }
 
